469f745560581b8eeb52adb579fa91070d15f179
Maxim <mkaramurzin02@gmail.com>
2023-09-11 15:07:02 -0700

    refactor(loan view): change the view containing loans into one of three tabs instead of being its own page

--- lib/screens/loan_view.dart
+++ lib/screens/loan_view.dart
@@ -16,10 +16,19 @@ class LoanView extends StatefulWidget {
   State<LoanView> createState() => _LoanViewState();
 }
 
-class _LoanViewState extends State<LoanView> {
+class _LoanViewState extends State<LoanView>
+    with SingleTickerProviderStateMixin {
   final AuthService _auth = AuthService();
   List<Loan> loanList = [];
   LoanFilter currentFilter = LoanFilter();
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController =
+        TabController(vsync: this, length: 3);
+  }
 
   Future<List<Loan>> fetchLoanList() async {
     if (_auth.user != null) {
@@ -49,9 +58,7 @@ class _LoanViewState extends State<LoanView> {
           context: context,
           builder: (context) => LoanFormDialog(
             onFormSubmit: () {
-              setState(() {
-                
-              });
+              setState(() {});
             },
           ),
         );
@@ -87,189 +94,203 @@ class _LoanViewState extends State<LoanView> {
           ),
         ),
         actions: [
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 20.0),
-            child: ElevatedButton(
-              onPressed: () {
-                menuOption(0);
-              },
-              child: Text('Create Loan'),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.white, // Background color
-                foregroundColor: Colors.blue, // Text color
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0),
-                ),
+          ElevatedButton(
+            onPressed: () {
+              menuOption(0);
+            },
+            child: Text('Create Loan'),
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.white, // Background color
+              foregroundColor: Colors.blue, // Text color
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
               ),
             ),
           ),
-          
+
           // TODO delete in production
           ElevatedButton(
             onPressed: () async {
               await Database(uid: _auth.user!.uid).totalReset();
-              setState(() {
-                
-              });
+              setState(() {});
             },
             child: Icon(Icons.autorenew),
           ),
 
-          PopupMenuButton<int>(
-              onSelected: (item) {
-                menuOption(item);
-                setState(() {});
-              },
-              icon: Icon(Icons.settings),
-              position: PopupMenuPosition.under,
-              itemBuilder: (context) => [
-                    PopupMenuItem(
-                      value: 0,
-                      child: Text('Create Loan'),
-                    ),
-                    PopupMenuItem(
-                      value: 1,
-                      child: Text('Apply Filter'),
-                    ),
-                    PopupMenuDivider(),
-                    PopupMenuItem(
-                      value: 2,
-                      child: Text("Sign Out"),
-                    ),
-                  ]),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: PopupMenuButton<int>(
+                onSelected: (item) {
+                  menuOption(item);
+                  setState(() {});
+                },
+                icon: Icon(Icons.settings, size: 30,),
+                position: PopupMenuPosition.under,
+                itemBuilder: (context) => [
+                      PopupMenuItem(
+                        value: 0,
+                        child: Text('Create Loan'),
+                      ),
+                      PopupMenuItem(
+                        value: 1,
+                        child: Text('Apply Filter'),
+                      ),
+                      PopupMenuDivider(),
+                      PopupMenuItem(
+                        value: 2,
+                        child: Text("Sign Out"),
+                      ),
+                    ]),
+          ),
         ],
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(icon: Icon(Icons.list, size: 18.0)),
+            Tab(icon: Icon(Icons.bar_chart, size: 18.0)),
+            Tab(icon: Icon(Icons.attach_money, size: 18.0)),
+          ],
+        ),
       ),
-      body: FutureBuilder<List<Loan>>(
-        future: fetchLoanList(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          }
-          if (snapshot.hasError) {
-            return Center(child: Text('Error fetching loans.'));
-          }
+      body: TabBarView(controller: _tabController, children: [
+        FutureBuilder<List<Loan>>(
+          future: fetchLoanList(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('Error fetching loans.'));
+            }
 
-          loanList = snapshot.data ?? [];
+            loanList = snapshot.data ?? [];
 
-          return Column(
-            children: [
-              Expanded(
-                child: Center(
-                  child: loanList.isEmpty
-                      ? Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text('No loans found',
-                                style: TextStyle(fontSize: 22)),
-                            SizedBox(
-                              height: 12,
-                            ),
-                            Container(
-                              width: 300,
-                              child: OutlinedButton(
-                                onPressed: () {
-                                  menuOption(0);
-                                },
-                                child: Text(
-                                  'Create a Loan',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                style: ButtonStyle(
-                                  side: MaterialStateProperty.resolveWith(
-                                      (states) => BorderSide(
-                                          color: Colors.blue, width: 2)),
-                                  padding: MaterialStateProperty.resolveWith(
-                                      (states) => EdgeInsets.symmetric(
-                                          vertical: 15.0, horizontal: 25.0)),
-                                  textStyle: MaterialStateProperty.resolveWith(
-                                      (states) => TextStyle(fontSize: 18)),
+            return Column(
+              children: [
+                Expanded(
+                  child: Center(
+                    child: loanList.isEmpty
+                        ? Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text('No loans found',
+                                  style: TextStyle(fontSize: 22)),
+                              SizedBox(
+                                height: 12,
+                              ),
+                              Container(
+                                width: 300,
+                                child: OutlinedButton(
+                                  onPressed: () {
+                                    menuOption(0);
+                                  },
+                                  child: Text(
+                                    'Create a Loan',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  style: ButtonStyle(
+                                    side: MaterialStateProperty.resolveWith(
+                                        (states) => BorderSide(
+                                            color: Colors.blue, width: 2)),
+                                    padding: MaterialStateProperty.resolveWith(
+                                        (states) => EdgeInsets.symmetric(
+                                            vertical: 15.0, horizontal: 25.0)),
+                                    textStyle:
+                                        MaterialStateProperty.resolveWith(
+                                            (states) =>
+                                                TextStyle(fontSize: 18)),
+                                  ),
                                 ),
                               ),
+                              Container(
+                                margin: EdgeInsets.only(top: 10),
+                                width: 300,
+                                child: OutlinedButton(
+                                  onPressed: () {
+                                    menuOption(1);
+                                  },
+                                  child: Text(
+                                    'Apply Filter',
+                                    style: TextStyle(fontSize: 18),
+                                  ),
+                                  style: ButtonStyle(
+                                    side: MaterialStateProperty.resolveWith(
+                                        (states) => BorderSide(
+                                            color: Colors.blue, width: 2)),
+                                    padding: MaterialStateProperty.resolveWith(
+                                        (states) => EdgeInsets.symmetric(
+                                            vertical: 15.0, horizontal: 25.0)),
+                                    textStyle:
+                                        MaterialStateProperty.resolveWith(
+                                            (states) =>
+                                                TextStyle(fontSize: 18)),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        : ListView(
+                            padding: EdgeInsets.symmetric(
+                              horizontal: (screenWidth - contentWidth) /
+                                  2, // Center the content
                             ),
-                            Container(
-                              margin: EdgeInsets.only(top: 10),
-                              width: 300,
-                              child: OutlinedButton(
-                                onPressed: () {
-                                  menuOption(1);
-                                },
-                                child: Text(
-                                  'Apply Filter',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                                style: ButtonStyle(
-                                  side: MaterialStateProperty.resolveWith(
-                                      (states) => BorderSide(
-                                          color: Colors.blue, width: 2)),
-                                  padding: MaterialStateProperty.resolveWith(
-                                      (states) => EdgeInsets.symmetric(
-                                          vertical: 15.0, horizontal: 25.0)),
-                                  textStyle: MaterialStateProperty.resolveWith(
-                                      (states) => TextStyle(fontSize: 18)),
-                                ),
-                              ),
-                            )
-                          ],
-                        )
-                      : ListView(
-                          padding: EdgeInsets.symmetric(
-                            horizontal: (screenWidth - contentWidth) /
-                                2, // Center the content
+                            children: loanList!
+                                .map((loan) => AnimatedContainer(
+                                      duration: Duration(milliseconds: 500),
+                                      width: contentWidth,
+                                      child: LoanItem(loan: loan),
+                                    ))
+                                .toList(),
                           ),
-                          children: loanList!
-                              .map((loan) => AnimatedContainer(
-                                    duration: Duration(milliseconds: 500),
-                                    width: contentWidth,
-                                    child: LoanItem(loan: loan),
-                                  ))
-                              .toList(),
-                        ),
+                  ),
                 ),
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.05,
-                color: Colors.blue,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Total Items'),
-                        Text(loanList.length.toString()),
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Average Principal'),
-                        Text(
-                            '\$${LoanLogic.calculateAverage('principal', loanList)}')
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Average Interest'),
-                        Text(
-                            '\$${LoanLogic.calculateAverage('interest', loanList)}')
-                      ],
-                    ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text('Average Duration'),
-                        Text(
-                            '${LoanLogic.calculateAverage('duration', loanList)} Days')
-                      ],
-                    ),
-                  ],
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                  color: Colors.blue,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Total Items'),
+                          Text(loanList.length.toString()),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Average Principal'),
+                          Text(
+                              '\$${LoanLogic.calculateAverage('principal', loanList)}')
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Average Interest'),
+                          Text(
+                              '\$${LoanLogic.calculateAverage('interest', loanList)}')
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('Average Duration'),
+                          Text(
+                              '${LoanLogic.calculateAverage('duration', loanList)} Days')
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
-      ),
+              ],
+            );
+          },
+        ),
+        Center(child: Text('This is the Numbers Tab')),
+        Center(child: Text('This is the Expenses Tab')),
+      ]),
     );
   }
 }
