5fabeb0ffd2b44c8216edced4c623b51df6da6e4
Maxim <mkaramurzin02@gmail.com>
2023-10-24 01:07:23 -0700

    refactor(excel export): Fix order of attributes; Add commented out code for exporting a second metrics tab.

--- lib/services/excel_export.dart
+++ lib/services/excel_export.dart
@@ -1,8 +1,14 @@
 import 'package:borrowbreeze/models/loan.dart';
+import 'package:borrowbreeze/services/auth.dart';
+import 'package:borrowbreeze/services/database.dart';
+import 'package:borrowbreeze/services/loan_logic.dart';
 import 'package:excel/excel.dart';
 import 'package:intl/intl.dart';
 
 class ExcelExportService {
+  final database = Database(uid: AuthService().user!.uid);
+  final logic = LoanLogic();
+
   Future<void> exportLoansToExcel(List<Loan> loans) async {
     // Create a new Excel file
     var excel = Excel.createExcel();
@@ -44,13 +50,13 @@ class ExcelExportService {
         loan.status,
         loan.lenderAccount,
         loan.financialPlatform,
-        loan.borrowerUsername,
         loan.borrowerName,
+        loan.borrowerUsername,
+        loan.amountRepaid.toString(),
         loan.principal.toString(),
         loan.repayAmount.toString(),
         loan.interest.toString(),
         loan.roi.toString(),
-        loan.amountRepaid.toString(),
         loan.originationDate.toDate().toString(),
         loan.repayDate.toDate().toString(),
         loan.duration.toString(),
@@ -70,8 +76,54 @@ class ExcelExportService {
       }
     }
 
+    // might need
+    // var metricsSheet = excel['Metrics'];
+    // Map<String, dynamic> metricsData = await fetchMetricsData();
+
+    // int rowIndex = 0;
+    // metricsData.forEach((key, value) {
+    //   metricsSheet
+    //       .cell(CellIndex.indexByColumnRow(columnIndex: 0, rowIndex: rowIndex))
+    //       .value = key;
+    //   metricsSheet
+    //       .cell(CellIndex.indexByColumnRow(columnIndex: 2, rowIndex: rowIndex))
+    //       .value = value
+    //           is double
+    //       ? (key.contains("ROI") || key.contains("Rate")
+    //           ? '${(value * 100).toStringAsFixed(2)}%'
+    //           : '\$${value.toStringAsFixed(2)}')
+    //       : value.toString();
+    //   rowIndex++;
+    // });
+
     // Save the Excel file
     String formattedDate = DateFormat('yyyy-MM-dd').format(DateTime.now());
     excel.save(fileName: "Loans_$formattedDate.xlsx");
+  }
+
+  Future<Map<String, dynamic>> fetchMetricsData() async {
+    return {
+      "Total Loans": await database.getTotalLoans(),
+      "Total Completed Loans": await database.getTotalCompletedLoans(),
+      "Owner Equity": await database.getOwnerEquity(),
+      "Available Liquid": await logic.calculateAvailableLiquid(),
+      "Total Money Lent": await database.getTotalMoneyLent(),
+      "Total Money Repaid": await database.getTotalMoneyRepaid(),
+      "Total Interest": await database.getTotalInterest(),
+      "Total Profit": await database.getTotalProfit(),
+      "ROI": await logic.calculateTotalROI(),
+      "Total Defaults": await database.getTotalDefaults(),
+      "Total Defaulted Money": await database.getTotalDefaulted(),
+      "Default Rate": await logic.calculateDefaultRate(),
+      "Expenses": await database.getTotalExpenses(),
+      "Pending Chargebacks": await database.getTotalPendingChargebacks(),
+      "Total Money Settled": await database.getTotalMoneySettled(),
+      "Funds Out In Loan": await database.getFundsInLoan(),
+      "Operational Profit": await logic.calculateOperationalProfit(),
+      "Operational ROI": await logic.calculateOperationalROI(),
+      "Projected Liquid": await logic.calculateProjectedLiquid(),
+      "Projected Profit": await database.getProjectedProfit(),
+      "Projected ROI": await logic.calculateProjectedROI(),
+    };
   }
 }
